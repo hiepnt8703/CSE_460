02ba0dbea55ec3192e4ed1e87f4c33bada9fcf33
hiepnt8703 <hiepnt8703@gmail.com>
2024-06-25 16:36:12 +0700

    commit_js

--- js/detail.js
+++ js/detail.js
@@ -1,12 +1,14 @@
-const buttons = document.querySelectorAll('.btnsize1');
+// js/main.js
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.btnsize1');
 
-        // Thêm sự kiện click cho mỗi nút
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                // Loại bỏ class 'selected' khỏi tất cả các nút
-                buttons.forEach(btn => btn.classList.remove('selected'));
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Loại bỏ class 'selected' khỏi tất cả các nút
+            buttons.forEach(btn => btn.classList.remove('selected'));
 
-                // Thêm class 'selected' cho nút được nhấp vào
-                button.classList.add('selected');
-            });
+            // Thêm class 'selected' cho nút được nhấp vào
+            button.classList.add('selected');
         });
+    });
+});
