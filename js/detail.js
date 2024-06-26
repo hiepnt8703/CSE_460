f2a025bee9b2cbccd91d92e88266fd5c2e89a228
hiepnt8703 <hiepnt8703@gmail.com>
2024-06-26 23:19:28 +0700

    abc

--- js/detail.js
+++ js/detail.js
@@ -6,9 +6,28 @@ document.addEventListener('DOMContentLoaded', function () {
         button.addEventListener('click', function () {
             // Loại bỏ class 'selected' khỏi tất cả các nút
             buttons.forEach(btn => btn.classList.remove('selected'));
+            
+            // Thêm class 'selected' cho nút được nhấp vào
+            button.classList.add('selected');
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.btnsize1');
+    const addToCartButton = document.getElementById('add-to-cart');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Loại bỏ class 'selected' khỏi tất cả các nút
+            buttons.forEach(btn => btn.classList.remove('selected'));
 
             // Thêm class 'selected' cho nút được nhấp vào
             button.classList.add('selected');
         });
+    });
+
+    addToCartButton.addEventListener('click', function () {
+        alert('Sản phẩm đã được thêm vào giỏ hàng!');
     });
 });
