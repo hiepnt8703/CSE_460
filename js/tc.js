e61130086bf962c2894791fa566c019d393ededa
hiepnt8703 <hiepnt8703@gmail.com>
2024-06-27 00:09:16 +0700

    update_trangchu

--- js/tc.js
+++ js/tc.js
@@ -16,3 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
     menuItems[0].classList.add("active");
     sections[0].classList.add("active");
 });
+
+
+document.getElementById("search-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của form
+
+    var searchInput = document.getElementById("search-input").value.trim();
+
+    if (searchInput === "") {
+        alert("Vui lòng nhập từ khóa tìm kiếm");
+    } else if (searchInput.toLowerCase() === "cà phê đen đá") {
+        window.location.href = "search.html";
+    } else {
+        alert("Không tìm thấy kết quả cho từ khóa tìm kiếm này");
+    }
+});
